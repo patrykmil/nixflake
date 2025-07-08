cd104896611d20832732cef7f6d1fa2d3c7fb62a
patrykmil <patryk.miler@student.pk.edu.pl>
2025-07-08 18:33:55 +0200

    Add nrs, hms aliasses

--- home-manager/modules/fish.nix
+++ home-manager/modules/fish.nix
@@ -8,6 +8,9 @@
       e = "eza --icons=always -a -l --git -B";
       es = "eza --icons=always -a -l --git -B --total-size";
       et = "eza --icons=auto -T -L=4 -a -l --git -B --no-filesize --no-permissions --octal-permissions --no-user --no-time --git-ignore";
+      nrs = "sudo nixos-rebuild switch --flake /home/ptrk/flakes";
+      hms = "home-manager switch --flake /home/ptrk/flakes -b backup";
+
     };
     functions = {
       fish_greeting = "";
