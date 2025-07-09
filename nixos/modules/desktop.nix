617613574c0770947bdac5198ed9059c5810bd56
patrykmil <patryk.miler@student.pk.edu.pl>
2025-07-09 17:27:14 +0200

    Add autologin

--- nixos/modules/desktop.nix
+++ nixos/modules/desktop.nix
@@ -1,6 +1,11 @@
+{ user, ... }:
 {
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.autoLogin = {
+    enable = true;
+    user = user;
+  };
   services.desktopManager.plasma6.enable = true;
   services.xserver.xkb = {
     layout = "us";
