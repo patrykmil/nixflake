798294ea3f8008180f3d20e25dcc05b295066aa3
patrykmil <patryk.miler@student.pk.edu.pl>
2025-07-10 19:36:34 +0200

    Config hyprpanel

--- home-manager/modules/hypr/hyprpanel.nix
+++ home-manager/modules/hypr/hyprpanel.nix
@@ -1,4 +1,177 @@
 {
   programs.hyprpanel.enable = true;
 
+  programs.hyprpanel.settings = {
+    bar = {
+      customModules = {
+        storage = {
+          paths = [ "/" ];
+        };
+      };
+      layouts = {
+        "0" = {
+          left = [
+            "dashboard"
+            "workspaces"
+            "windowtitle"
+          ];
+          right = [
+            "volume"
+            "network"
+            "systray"
+            "clock"
+            "notifications"
+          ];
+        };
+        "1" = {
+          right = [
+            "workspaces"
+            "windowtitle"
+          ];
+        };
+      };
+      location = "bottom";
+      buttons = {
+        enableBorders = false;
+        dashboard = {
+          enableBorder = true;
+        };
+        workspaces = {
+          enableBorder = false;
+        };
+        volume = {
+          enableBorder = false;
+        };
+        network = {
+          enableBorder = false;
+        };
+        systray = {
+          enableBorder = false;
+        };
+        clock = {
+          enableBorder = false;
+          spacing = "0.3em";
+        };
+      };
+      border = {
+        location = "top";
+        width = "0.05em";
+      };
+      outer_spacing = "1.5em";
+      launcher = {
+        icon = "󱄅";
+        autoDetectIcon = false;
+      };
+      workspaces = {
+        show_icons = false;
+        show_numbered = false;
+        monitorSpecific = true;
+        workspaceMask = false;
+        showWsIcons = true;
+        showApplicationIcons = true;
+        numbered_active_indicator = "underline";
+      };
+      network = {
+        showWifiInfo = true;
+      };
+      clock = {
+        format = "%a %d %b %H:%M";
+        showIcon = false;
+        showTime = true;
+      };
+      media = {
+        show_label = true;
+        truncation = true;
+        truncation_size = 30;
+      };
+      notifications = {
+        show_total = true;
+        hideCountWhenZero = true;
+      };
+    };
+    theme = {
+      bar = {
+        floating = false;
+        menus = {
+          monochrome = false;
+        };
+        location = "bottom";
+        buttons = {
+          enableBorders = false;
+          dashboard = {
+            enableBorder = true;
+          };
+          workspaces = {
+            enableBorder = false;
+          };
+          volume = {
+            enableBorder = false;
+          };
+          network = {
+            enableBorder = false;
+          };
+          systray = {
+            enableBorder = false;
+          };
+          clock = {
+            enableBorder = false;
+            spacing = "0.3em";
+          };
+        };
+        border = {
+          location = "top";
+          width = "0.05em";
+        };
+        outer_spacing = "1.5em";
+        menus = {
+          menu = {
+            power = {
+              scaling = 90;
+            };
+            dashboard = {
+              profile = {
+                size = "8.5em";
+              };
+            };
+          };
+        };
+      };
+      font = {
+        name = "MesloLGS NF";
+        label = "MesloLGS NF";
+        size = "1.1rem";
+        weight = 500;
+      };
+    };
+    wallpaper = {
+      pywal = false;
+    };
+    menus = {
+      clock = {
+        weather = {
+          location = "Kraków";
+          unit = "metric";
+        };
+        time = {
+          military = true;
+        };
+      };
+      dashboard = {
+        shortcuts = {
+          enabled = false;
+        };
+        controls = {
+          enabled = true;
+        };
+        stats = {
+          enabled = false;
+        };
+        powermenu = {
+          confirmation = true;
+        };
+      };
+      transition = "crossfade";
+    };
+    scalingPriority = "hyprland";
+  };
 }
--- nixos/packages.nix
+++ nixos/packages.nix
@@ -31,5 +31,6 @@
 
   fonts.packages = with pkgs; [
     meslo-lg
+    meslo-lgs-nf
   ];
 }
