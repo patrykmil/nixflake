452e9a225df52f3053fc9517c9bac5baaf64a183
patrykmil <patryk.miler@student.pk.edu.pl>
2025-07-18 19:10:48 +0200

    Use relative path for wallpapers

--- home-manager/modules/wallpaper/hyprpaper.nix
+++ home-manager/modules/wallpaper/hyprpaper.nix
@@ -1,3 +1,6 @@
+let
+  wallpaperFolder = builtins.toString ./wallpapers;
+in
 {
   services.hyprpaper = {
     enable = true;
@@ -5,11 +8,11 @@
       splash = false;
 
       preload = [
-        "/home/ptrk/flakes/wallpapers/ink_wave.png"
+        "${wallpaperFolder}/ink_wave.png"
       ];
 
       wallpaper = [
-        ",/home/ptrk/flakes/wallpapers/ink_wave.png"
+        ",${wallpaperFolder}/ink_wave.png"
       ];
     };
   };
