55662c06d6f3c037c4e5287dd8546961a36c6aba
patrykmil <patryk.miler@student.pk.edu.pl>
2025-07-27 21:01:18 +0200

    Update home-packages for device specific apps

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -1,21 +1,38 @@
-{ pkgs, ... }:
+{ pkgs, hostName, ... }:
 {
   nixpkgs.config.allowUnfree = true;
 
-  home.packages = with pkgs; [
-    fastfetch
-    nixfmt-rfc-style
-    networkmanagerapplet
-    mako
-    pulseaudio
-    pamixer
-    pavucontrol
-    alacritty
-    geeqie
-    hyprpicker
-    hyprpaper
-    jetbrains.pycharm-professional
-    bun
-    dbeaver-bin
-  ];
+  home.packages =
+    with pkgs;
+    [
+      fastfetch
+      nixfmt-rfc-style
+      networkmanagerapplet
+      mako
+      pulseaudio
+      pamixer
+      pavucontrol
+      alacritty
+      geeqie
+      hyprpicker
+      hyprpaper
+      jetbrains.pycharm-professional
+      bun
+      dbeaver-bin
+    ]
+    ++ (
+      if hostName == "desktop" then
+        [
+          jetbrains.pycharm-professional
+          dbeaver-bin
+        ]
+      else if hostName == "laptop" then
+        [
+
+        ]
+      else
+        [
+
+        ]
+    );
 }
