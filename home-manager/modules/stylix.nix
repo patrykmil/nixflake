5c8d649023852d6eea39924af7b9c81ff7f7b688
patrykmil <patryk.miler@student.pk.edu.pl>
2025-08-17 15:11:46 +0200

    Change style to catppuccin-mocha

--- home-manager/modules/stylix.nix
+++ home-manager/modules/stylix.nix
@@ -2,6 +2,9 @@
 {
   stylix = {
     enable = true;
+
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
     image = ../../wallpapers/ink-wave.jpg;
     polarity = "dark";
 
