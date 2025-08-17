7206e0329a4fa0392c2f0d39f3f4655b2d433d3e
patrykmil <patryk.miler@student.pk.edu.pl>
2025-08-17 14:47:08 +0200

    Change sensitivity for all mice

--- home-manager/modules/hyprland/inputs.nix
+++ home-manager/modules/hyprland/inputs.nix
@@ -3,14 +3,9 @@
     kb_layout = "pl";
     numlock_by_default = true;
     follow_mouse = 1;
-    sensitivity = 0;
+    sensitivity = -0.5;
     touchpad = {
       natural_scroll = false;
     };
-  };
-
-  device = {
-    name = "logitech-g403-1";
-    sensitivity = -0.5;
   };
 }
