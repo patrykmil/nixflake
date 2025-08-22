b7acfdd7b0b238e5244eac8c38f3cd53bc0c1f8c
patrykmil <patryk.miler@student.pk.edu.pl>
2025-08-22 17:55:16 +0200

    Add sensitivity for vxe

--- home-manager/modules/hyprland/sensitivity.nix
+++ home-manager/modules/hyprland/sensitivity.nix
@@ -5,6 +5,10 @@
       sensitivity = -0.5;
     }
     device {
+      name = compx-vxe-nordicmouse-1k-dongle-1
+      sensitivity = -0.5;
+    }
+    device {
       name = dell09ed:00-27c6:01e0-touchpad
       sensitivity = -0.1;
     }
