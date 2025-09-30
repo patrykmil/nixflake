fdf1c6bc97a94d859781f1edbfa6975aa6cedea0
patrykmil <patryk.miler@student.pk.edu.pl>
2025-09-30 15:22:38 +0200

    Change iwd to wpa_supplicant to fix wifi

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -2,14 +2,5 @@
 {
   networking.networkmanager.enable = true;
   networking.hostName = hostName;
-  networking.wireless.iwd.enable = true;
-  networking.networkmanager.wifi.backend = "iwd";
-  networking.wireless.iwd.settings = {
-    IPv6 = {
-      Enabled = true;
-    };
-    Settings = {
-      AutoConnect = true;
-    };
-  };
+  networking.networkmanager.wifi.backend = "wpa_supplicant";
 }
