0be2063060fc0062c9b5d378a74a333f06674f7a
patrykmil <patryk.miler@student.pk.edu.pl>
2025-10-17 18:02:07 +0200

    Fix online printing

--- system/modules/printing.nix
+++ system/modules/printing.nix
@@ -1,3 +1,10 @@
+{ pkgs, ... }:
 {
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+  services.printing.drivers = [ pkgs.hplip ];
 }
