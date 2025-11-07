623c0a1660670979c990d4405b83dc86ce7f113f
patrykmil <patryk.miler@student.pk.edu.pl>
2025-11-07 20:13:37 +0100

    Make shell aliases be hostname dependent

--- home-manager/modules/shell.nix
+++ home-manager/modules/shell.nix
@@ -1,3 +1,4 @@
+{ hostName, ... }:
 {
   programs.fish = {
     enable = true;
@@ -10,15 +11,23 @@
       es = "eza --icons=always -a -l --git -B --total-size";
       et = "eza --icons=auto -T -L=4 -a -l --git -B --no-filesize --no-permissions --octal-permissions --no-user --no-time --git-ignore";
 
-      dss = "sudo nixos-rebuild switch --flake /home/ptrk/flakes#desktop";
-      dhs = "home-manager switch --flake /home/ptrk/flakes#ptrk-desktop -b backup";
-      lss = "sudo nixos-rebuild switch --flake /home/ptrk/flakes#laptop";
-      lhs = "home-manager switch --flake /home/ptrk/flakes#ptrk-laptop -b backup";
-
-      dst = "sudo nixos-rebuild test --flake /home/ptrk/flakes#desktop";
-      lst = "sudo nixos-rebuild test --flake /home/ptrk/flakes#laptop";
-
-    };
+    }
+    // (
+      if hostName == "desktop" then
+        {
+          dss = "sudo nixos-rebuild switch --flake /home/ptrk/flakes#desktop";
+          dhs = "home-manager switch --flake /home/ptrk/flakes#ptrk-desktop -b backup";
+          dst = "sudo nixos-rebuild test --flake /home/ptrk/flakes#desktop";
+        }
+      else if hostName == "laptop" then
+        {
+          lss = "sudo nixos-rebuild switch --flake /home/ptrk/flakes#laptop";
+          lhs = "home-manager switch --flake /home/ptrk/flakes#ptrk-laptop -b backup";
+          lst = "sudo nixos-rebuild test --flake /home/ptrk/flakes#laptop";
+        }
+      else
+        { }
+    );
     functions = {
       fish_greeting = "";
     };
