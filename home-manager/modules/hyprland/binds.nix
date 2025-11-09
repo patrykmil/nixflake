0b4e4c186d07d17644c829c4540eaf4be6287746
patrykmil <patryk.miler@student.pk.edu.pl>
2025-11-09 17:58:00 +0100

    Add waybar toggle

--- home-manager/modules/hyprland/binds.nix
+++ home-manager/modules/hyprland/binds.nix
@@ -10,6 +10,8 @@
     "$mainMod,T,exec,alacritty"
     "$mainMod,B,exec,brave"
     "$mainMod,E,exec,nautilus --new-window"
+    "$mainMod,M,exec,pkill waybar || nohup waybar > /dev/null 2>&1 &"
+
 
     "$mainMod,Q,killactive,"
     "$mainMod,F,togglefloating,"
