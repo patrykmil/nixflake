2dc5be118e1b58fb420def886e81467f54acfced
patrykmil <patryk.miler@student.pk.edu.pl>
2026-01-07 20:12:26 +0100

    Add git aliases

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -6,11 +6,15 @@
         email = "patryk.miler@student.pk.edu.pl";
         name = "patrykmil";
       };
-      core.editor = "code";
+      core.editor = "hx";
       pull.rebase = "true";
       init.defaultBranch = "main";
       alias = {
         amend = "commit --amend --no-edit";
+        graph = "log --graph --all";
+        myclone = "!f() { git clone git@github.com:patrykmil/$1.git; }; f";
+        unadd = "restore --staged";
+        wdiff = "diff -w --word-diff=color --ignore-space-at-eol";
       };
     };
   };
