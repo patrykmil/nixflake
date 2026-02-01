43a5182cc03a010f66e7f660fe2d5240337fc4e1
patrykmil <patryk.miler@student.pk.edu.pl>
2026-02-01 14:25:41 +0100

    Disable DoH for brave

--- system/modules/brave-policies.nix
+++ system/modules/brave-policies.nix
@@ -16,10 +16,11 @@
       "AutofillAddressEnabled": false,
       "AutofillCreditCardEnabled": false,
       "DnsOverHttpsMode": "secure",
-      "DnsOverHttpsTemplates": "https://base.dns.mullvad.net/dns-query"
     }
   '';
 }
 
+      # "DnsOverHttpsTemplates": "https://dnsforge.de/dns-query"
 # https://base.dns.mullvad.net/dns-query
 # https://security.cloudflare-dns.com/dns-query
+# https://dnsforge.de/dns-query
