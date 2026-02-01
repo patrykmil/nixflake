8b4c76960001c4d94600044fc70a67cd46e614d3
patrykmil <patryk.miler@student.pk.edu.pl>
2026-02-01 14:26:07 +0100

    Add PROTON-GE path for steam

--- system/modules/games.nix
+++ system/modules/games.nix
@@ -4,4 +4,8 @@
   environment.systemPackages = with pkgs; [
     heroic
   ];
+
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.config/heroic/tools/proton";
+  };
 }
