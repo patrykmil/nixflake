05792c2aee1ceecf7a119e4ef19e998d9a69ddf8
patrykmil <patryk.miler@student.pk.edu.pl>
2026-02-08 18:33:38 +0100

    Fix waybar module scripts paths

--- modules/waybar/waybar-modules.nix
+++ modules/waybar/waybar-modules.nix
@@ -20,7 +20,7 @@ in
   "custom/clock2" = {
     interval = 16;
     return-type = "json";
-    exec = "python3 ${flakeDir}/modules/bar/waybar/cal.py";
+    exec = "python3 ${flakeDir}/modules/waybar/cal.py";
   };
 
   pulseaudio = {
@@ -108,7 +108,7 @@ in
   };
 
   "custom/tray" = {
-    exec = "${flakeDir}/modules/bar/waybar/tray.sh";
+    exec = "${flakeDir}/modules/waybar/tray.sh";
     interval = 5;
     format = "{}";
   };
