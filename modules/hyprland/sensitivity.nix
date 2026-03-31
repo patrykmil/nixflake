ee45f15e380fda45d3a41c7c949e4fb64b77b6ba
patrykmil <patryk.miler@student.pk.edu.pl>
2026-03-31 20:28:04 +0200

    Update sensitivity for plugged-in vxe

--- modules/hyprland/sensitivity.nix
+++ modules/hyprland/sensitivity.nix
@@ -9,6 +9,14 @@
       sensitivity = -0.5
     }
     device {
+      name = compx-vxe-r1-pro-max-consumer-control-1
+      sensitivity = -0.5
+    }
+    device {
+      name = compx-vxe-r1-pro-max-1
+      sensitivity = -0.5
+    }
+    device {
       name = dell09ed:00-27c6:01e0-touchpad
       sensitivity = 0.1
     }
