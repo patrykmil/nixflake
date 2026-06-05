a84f3fa72e5482ce7688343a25839245f5c2ebde
patrykmil <patryk.miler@student.pk.edu.pl>
2026-06-05 20:18:07 +0200

    Remove unnecessary networking options

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,10 +3,4 @@
   networking.networkmanager.enable = true;
   networking.hostName = hostName;
   networking.networkmanager.wifi.backend = "wpa_supplicant";
-  networking.networkmanager.settings = {
-    connection = {
-      "wifi.powersave" = "2";
-    };
-  };
-  boot.kernelParams = [ "usbcore.autosuspend=-1" ];
 }
