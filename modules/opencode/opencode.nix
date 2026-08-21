5ac92a1b428c130af3fc3dedc8d04629a88cf1ba
patrykmil <patryk.miler@student.pk.edu.pl>
2026-08-21 18:53:31 +0200

    Update opencode

--- modules/opencode/opencode.nix
+++ modules/opencode/opencode.nix
@@ -1,11 +1,33 @@
 { pkgs-unstable, config, ... }:
 let
   flakeDir = "${config.home.homeDirectory}/flakes";
+
+  version = "1.18.21";
+  src = pkgs-unstable.fetchFromGitHub {
+    owner = "anomalyco";
+    repo = "opencode";
+    tag = "v${version}";
+    hash = "sha256-WKG/lts+wzDjYJ5pOZ0X4Kb0rJ1TzYQzQgjyQBY+bxs=";
+  };
+
+  opencode-pinned =
+    (pkgs-unstable.opencode.overrideAttrs (_: {
+      inherit version src;
+    })).overrideAttrs (old: {
+      passthru = old.passthru // {
+        node_modules = old.passthru.node_modules.overrideAttrs (_: {
+          outputHash = "sha256-WqEZQCVl4oQFVbrhlWVaBW+JiSqjSK+LILPkDV9Avds=";
+        });
+      };
+    });
 in
 {
-  home.packages = with pkgs-unstable; [
-    opencode
-  ];
+  home.packages =
+    with pkgs-unstable;
+    [
+      opencode-desktop
+    ]
+    ++ [ opencode-pinned ];
 
   xdg.configFile."opencode/tui.jsonc".source =
     config.lib.file.mkOutOfStoreSymlink "${flakeDir}/modules/opencode/tui.jsonc";
